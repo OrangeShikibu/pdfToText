5e38f9be6cba1c3c80fb5f734d8298d0aa7b5c55
OrangeShikibu <yumotomichiaki@gmail.com>
2022-05-19 13:22:47 +0900

    施策版として作成

--- test2.py
+++ test2.py
@@ -1,4 +1,8 @@
 from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
 import os
 
 rmgr = PDFResourceManager
